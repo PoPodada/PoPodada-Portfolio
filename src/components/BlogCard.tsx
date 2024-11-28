7655f5e00fcb2cb8e0de74ecf9c93e45b77386a7
PoPodada <goito.722@gmail.com>
2024-11-28 11:25:01 +0900

    ブログサムネの追加

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { formatDate } from "../utils/Date";
 
 type BlogCardProps = {
 	id: string;
@@ -12,7 +13,15 @@ const BlogCard = (props: BlogCardProps) => {
   return (
     <Link href={`/${props.id}`} className="w-[300px] mx-6 my-2">
       <div className="bg-white rounded-lg shadow-md overflow-hidden w-[300px]">
-        <div className="h-[150px] bg-gray-200"></div>
+        <div className="h-[150px] bg-gray-200">
+        <Image
+              src={props.imgURL ? props.imgURL: ""}
+              width={300}
+              height={150}
+              alt=""
+              className=""
+            />
+        </div>
         <div className="px-1">
           <div className="flex">
             <Image
@@ -20,9 +29,9 @@ const BlogCard = (props: BlogCardProps) => {
               width={20}
               height={20}
               alt=""
-              className="opacity-50 mr-1"
+              className="opacity-50 mr-1 mt-1"
             />
-            <p className="opacity-50 font-bold">{props.createdAt}</p>
+            <p className="opacity-50 font-bold mt-1">{formatDate(props.createdAt)}</p>
           </div>
           <div className="h-[80px] ml-1">
             <h2 className="text-lg font-bold">{props.title}</h2>
